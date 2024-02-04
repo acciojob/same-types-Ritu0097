defb48c81337ca0d437bdb8062dac07460f0917c
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-04 14:52:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function isSameType(value1, value2) {
-  if(isNaN(value1)&&isNaN(value2)){
+  if(typeof(value1)===typeof(value2)){
 	  return true;
   }
-	else{
-		return typeof value1 === typeof value2;
+	else if(typeof(value1)==isNaN()||typeof(value2)==isNaN()){
+		false;
 	}
 }
 
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+// let value1 = prompt("Enter Start of the Range.");
+// let value2 = prompt("Enter End Of the Range.");
+// alert(isSameType(value1, value2));
