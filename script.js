064108e4c8cdd622eccd9bdca72a712a4957cac3
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-04 14:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function isSameType(value1, value2) {
-  if(typeof(value1)===typeof(value2)){
+  if(isNaN(value1)&&isNaN(value2)){
 	  return true;
   }
 	else{
-		return false;
+		return typeof value1 === typeof value2;
 	}
 }
 
